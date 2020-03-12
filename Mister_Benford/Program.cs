d48900cff9e47e3e7c07a9010d6bc7097d07d78f
steffe21dev <stefan.smudja0001@stud.hkr.se>
2020-03-12 18:02:34 +0100

    Felhantering och utskrift av bodien

--- Mister_Benford/Program.cs
+++ Mister_Benford/Program.cs
@@ -13,7 +13,7 @@ namespace Mister_Benford
         static void Main(string[] args)
         {
 
-            Console.WriteLine(FiggleFonts.Ogre.Render("Benford"));
+            Console.WriteLine(FiggleFonts.Pawp.Render("Benfords Lag"));
 
 
             double ettor = 0;
@@ -30,107 +30,122 @@ namespace Mister_Benford
 
             bool mellanrum = false;
 
-
-            //Knappa in urlen
-            string url = "https://en.wikipedia.org/wiki/Number";
-
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load(url);
-
-            var htmlbody = doc.DocumentNode.SelectSingleNode("//body");
-
-
-            string body = htmlbody.InnerText;
-
-            
-            for(int i = 0; i < body.Length; i++)
+            //Felhantering
+            try
             {
-                switch (body[i])
-                {
-                    case '1':
-                        if (mellanrum == false)
-                        {
-                            ettor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '2':
-                        if (mellanrum == false)
-                        {
-                            tvåor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '3':
-                        if (mellanrum == false)
-                        {
-                            treor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '4':
-                        if (mellanrum == false)
-                        {
-                            fyror++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '5':
-                        if (mellanrum == false)
-                        {
-                            femmor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '6':
-                        if (mellanrum == false)
-                        {
-                            sexor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '7':
-                        if (mellanrum == false)
-                        {
-                            sjuor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '8':
-                        if (mellanrum == false)
-                        {
-                            åttor++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case '9':
-                        if (mellanrum == false)
-                        {
-                            nior++;
-                            totalt++;
-                            mellanrum = true;
-                        }
-                        break;
-                    case ' ':
-                        mellanrum = false;
-                        break;
-                }
-            }
+                //Knappa in urlen
+                string url = "din länk";
 
-            
-            Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n", ettor,tvåor,treor,fyror,femmor,sexor,sjuor,åttor,nior));
-            Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n", 
-                (double)((ettor / totalt) * 100), ((tvåor / totalt) * 100), ((treor / totalt) * 100), ((fyror / totalt) * 100),
-                ((femmor / totalt) * 100), ((sexor / totalt) * 100), ((sjuor / totalt) * 100), ((åttor / totalt) * 100), ((nior / totalt) * 100)));
-            Console.ReadLine();
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(url);
+
+                var htmlbody = doc.DocumentNode.SelectSingleNode("//body");
+
+
+                string body = htmlbody.InnerText;
+
+
+                for (int i = 0; i < body.Length; i++)
+                {
+                    switch (body[i])
+                    {
+                        case '1':
+                            if (mellanrum == false)
+                            {
+                                ettor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '2':
+                            if (mellanrum == false)
+                            {
+                                tvåor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '3':
+                            if (mellanrum == false)
+                            {
+                                treor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '4':
+                            if (mellanrum == false)
+                            {
+                                fyror++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '5':
+                            if (mellanrum == false)
+                            {
+                                femmor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '6':
+                            if (mellanrum == false)
+                            {
+                                sexor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '7':
+                            if (mellanrum == false)
+                            {
+                                sjuor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '8':
+                            if (mellanrum == false)
+                            {
+                                åttor++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case '9':
+                            if (mellanrum == false)
+                            {
+                                nior++;
+                                totalt++;
+                                mellanrum = true;
+                            }
+                            break;
+                        case ' ':
+                            mellanrum = false;
+                            break;
+                    }
+                }
+
+
+                Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n", ettor, tvåor, treor, fyror, femmor, sexor, sjuor, åttor, nior));
+                Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n",
+                    (double)((ettor / totalt) * 100), ((tvåor / totalt) * 100), ((treor / totalt) * 100), ((fyror / totalt) * 100),
+                    ((femmor / totalt) * 100), ((sexor / totalt) * 100), ((sjuor / totalt) * 100), ((åttor / totalt) * 100), ((nior / totalt) * 100)));
+
+                Console.WriteLine("\n Vill du få fram html bodien? Klicka enter");
+
+                Console.ReadLine();
+
+                Console.WriteLine(body);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
         }
     }
 }
