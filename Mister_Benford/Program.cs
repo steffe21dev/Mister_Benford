d3d20d6da4c55068da55a3a8d631c037fbcb329f
steffe21dev <stefan.smudja0001@stud.hkr.se>
2020-03-13 16:27:48 +0100

    Omskrivning

--- Mister_Benford/Program.cs
+++ Mister_Benford/Program.cs
@@ -10,144 +10,142 @@ namespace Mister_Benford
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine(FiggleFonts.Pawp.Render("Benfords Lag"));
-
-
-            double ettor = 0;
-            double tvåor = 0;
-            double treor = 0;
-            double fyror = 0;
-            double femmor = 0;
-            double sexor = 0;
-            double sjuor = 0;
-            double åttor = 0;
-            double nior = 0;
-
-            double totalt = 0;
-
-            bool mellanrum = false;
+            Console.WriteLine(FiggleFonts.Graceful.Render("Benfords Lag"));
 
             //Felhantering
             try
             {
                 //Knappa in urlen
-                string url = "https://www.random.org/integer-sets/?sets=1&num=5000&min=1&max=100000&seqnos=on&commas=on&sort=on&order=index&format=html&rnd=new";
+                string url = "https://www.nomoda.net/";
+                double[] tal;
 
                 HtmlWeb web = new HtmlWeb();
                 HtmlDocument doc = web.Load(url);
 
                 var htmlbody = doc.DocumentNode.SelectSingleNode("//body");
-
-
                 string body = htmlbody.InnerText;
 
 
-                for (int i = 0; i < body.Length; i++)
-                {
-                    switch (body[i])
-                    {
-                        case '1':
-                            if (mellanrum == false)
-                            {
-                                ettor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '2':
-                            if (mellanrum == false)
-                            {
-                                tvåor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '3':
-                            if (mellanrum == false)
-                            {
-                                treor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '4':
-                            if (mellanrum == false)
-                            {
-                                fyror++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '5':
-                            if (mellanrum == false)
-                            {
-                                femmor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '6':
-                            if (mellanrum == false)
-                            {
-                                sexor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '7':
-                            if (mellanrum == false)
-                            {
-                                sjuor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '8':
-                            if (mellanrum == false)
-                            {
-                                åttor++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case '9':
-                            if (mellanrum == false)
-                            {
-                                nior++;
-                                totalt++;
-                                mellanrum = true;
-                            }
-                            break;
-                        case ' ':
-                            mellanrum = false;
-                            break;
-                    }
-                }
+                tal = Kalkylera_FörstaSiffror(body);
 
 
-                Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n", ettor, tvåor, treor, fyror, femmor, sexor, sjuor, åttor, nior));
+                Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n", tal[1], tal[2], tal[3], tal[4], tal[5], tal[6], tal[7], tal[8], tal[9]));
                 Console.WriteLine(string.Format(" Antalet ettor: {0} \n Antalet tvåor: {1} \n Antalet treor: {2} \n Antalet fyror: {3} \n Antalet femmor: {4} \n Antalet sexor: {5} \n Antalet sjuor: {6} \n Antalet åttor: {7} \n Antalet nior: {8} \n",
-                    (double)((ettor / totalt) * 100), ((tvåor / totalt) * 100), ((treor / totalt) * 100), ((fyror / totalt) * 100),
-                    ((femmor / totalt) * 100), ((sexor / totalt) * 100), ((sjuor / totalt) * 100), ((åttor / totalt) * 100), ((nior / totalt) * 100)));
+                    (double)((tal[1] / tal[0]) * 100), ((tal[2] / tal[0]) * 100), ((tal[3] / tal[0]) * 100), ((tal[4] / tal[0]) * 100),
+                    ((tal[5] / tal[0]) * 100), ((tal[6] / tal[0]) * 100), ((tal[7] / tal[0]) * 100), ((tal[8] / tal[0]) * 100), ((tal[9] / tal[0]) * 100)));
 
-
-                Console.WriteLine("Totalt " + totalt + " tal");
+                Console.WriteLine("Totalt " + tal[0] + " tal");
                 Console.WriteLine("\n Vill du få fram html bodien? Klicka enter");
 
-                Console.ReadLine();
 
+                Console.ReadLine();
                 Console.WriteLine(body);
-
                 Console.ReadLine();
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 Console.ReadLine();
             }
+
+
+        }
+
+        static double[] Kalkylera_FörstaSiffror(string body)
+        {
+
+            double[] tal = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            bool mellanrum = false; 
+
+            for (int i = 0; i < body.Length; i++)
+            {
+                switch (body[i])
+                {
+                    case '1':
+                        if (mellanrum == false)
+                        {
+                            tal[1]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '2':
+                        if (mellanrum == false)
+                        {
+                            tal[2]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '3':
+                        if (mellanrum == false)
+                        {
+                            tal[3]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '4':
+                        if (mellanrum == false)
+                        {
+                            tal[4]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '5':
+                        if (mellanrum == false)
+                        {
+                            tal[5]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '6':
+                        if (mellanrum == false)
+                        {
+                            tal[6]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '7':
+                        if (mellanrum == false)
+                        {
+                            tal[6]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '8':
+                        if (mellanrum == false)
+                        {
+                            tal[8]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case '9':
+                        if (mellanrum == false)
+                        {
+                            tal[9]++;
+                            tal[0]++;
+                            mellanrum = true;
+                        }
+                        break;
+                    case ' ':
+                        mellanrum = false;
+                        break;
+                }
+            }
+
+            return tal;
         }
     }
 }
